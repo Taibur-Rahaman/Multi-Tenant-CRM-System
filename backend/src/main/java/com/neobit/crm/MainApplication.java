43a5b559bc8e7e73b4bb63771bc260a082fc6431
Taibur-Rahaman <94689383+Taibur-Rahaman@users.noreply.github.com>
2025-11-08 00:02:05 +0600

    Initial Spring Boot main application

--- backend/src/main/java/com/neobit/crm/MainApplication.java
+++ backend/src/main/java/com/neobit/crm/MainApplication.java
@@ -1,1 +1,13 @@
+package com.neobit.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+        System.out.println("🚀 Multi-Tenant CRM Backend Running...");
+    }
+}
 
